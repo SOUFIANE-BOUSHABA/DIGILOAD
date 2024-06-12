a29a2becf45b6c14cbf7a88dce661ca6c5be8022
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-06-12 16:37:42 +0000

    quick fix

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
+import DashboardCopy from './pages/DashboardCopy';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
 import PrivateRoute from './components/PrivateRoute';
@@ -15,6 +16,7 @@ const AppRouter = () => (
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Layout />}>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/DashboardCopy" element={<DashboardCopy />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/tableau" element={<Tableau />} />
           <Route path="/profile/update/:id" element={<UpdateProfile/>} />
