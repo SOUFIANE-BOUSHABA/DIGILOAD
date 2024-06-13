bd1fb7f71df2a6af72e0f8b35bb3bcf4881b57fd
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-06-13 19:20:13 +0000

    fix  assidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,7 +3,15 @@ import { NavLink , useLocation } from 'react-router-dom';
 import logo from '../assets/img/logo.png';
 import leftArrow from '../assets/img/left-arrow.svg';
 import dashboardIcon from '../assets/img/darhboard_alt.png';
+import tableIcon from '../assets/img/Table.png';
 import infoIcon from '../assets/img/Info_fill.png';
+import profileIcon from '../assets/img/User_fill.png';
+import organismeIcon from '../assets/img/Work.svg';
+import mailIcon from '../assets/img/Message_fill.png';
+import workflowIcon from '../assets/img/Send_fill.png';
+
+import analyticsIcon from '../assets/img/bar-chart.png';
+
 
 import '../assets/css/Sidebar.css';
 
@@ -28,22 +36,22 @@ const Sidebar = () => {
           <img src={dashboardIcon} alt="Dashboard" className="w-4 h-4" /> <span className="link-text">Dashboard</span>
         </NavLink>
         <NavLink to="/tableau" activeClassName="active-link" className={`py-2.5 px-4 text-gray-700 flex items-center gap-2 hover:rounded-tl-full hover:rounded-bl-full ${location.pathname === '/tableau' ? 'active-link' : ''}`}>
-          <img src={dashboardIcon} alt="Profile" className="w-4 h-4" /> <span className="link-text">Tableau</span>
+          <img src={tableIcon} alt="Profile" className="w-4 h-4" /> <span className="link-text">Tableau</span>
         </NavLink>
         <NavLink to="/profile" activeClassName="active-link" className={`py-2.5 px-4 text-gray-700 flex items-center gap-2 hover:rounded-tl-full hover:rounded-bl-full ${location.pathname === '/profile' ? 'active-link' : ''}`}>
-          <img src={dashboardIcon} alt="Profile" className="w-4 h-4" /> <span className="link-text">Profile</span>
+          <img src={profileIcon} alt="Profile" className="w-4 h-4" /> <span className="link-text">Profile</span>
         </NavLink>
         <NavLink to="/mail" activeClassName="active-link" className={`py-2.5 px-4 text-gray-700 flex items-center gap-2 hover:rounded-tl-full hover:rounded-bl-full ${location.pathname === '/mail' ? 'active-link' : ''}`}>
-          <img src={dashboardIcon} alt="Mail" className="w-4 h-4" /> <span className="link-text">Mail</span>
+          <img src={mailIcon} alt="Mail" className="w-4 h-4" /> <span className="link-text">Mail</span>
         </NavLink>
         <NavLink to="/workflow" activeClassName="active-link" className={`py-2.5 px-4 text-gray-700 flex items-center gap-2 hover:rounded-tl-full hover:rounded-bl-full ${location.pathname === '/workflow' ? 'active-link' : ''}`}>
-          <img src={dashboardIcon} alt="Workflow" className="w-4 h-4" /> <span className="link-text">Workflow</span>
+          <img src={workflowIcon} alt="Workflow" className="w-4 h-4" /> <span className="link-text">Workflow</span>
         </NavLink>
         <NavLink to="/analytics" activeClassName="active-link" className={`py-2.5 px-4 text-gray-700 flex items-center gap-2 hover:rounded-tl-full hover:rounded-bl-full ${location.pathname === '/analytics' ? 'active-link' : ''}`}>
-          <img src={dashboardIcon} alt="Analytics" className="w-4 h-4" /> <span className="link-text">Analytics</span>
+          <img src={analyticsIcon} alt="Analytics" className="w-4 h-4" /> <span className="link-text">Analytics</span>
         </NavLink>
         <NavLink to="/organisme" activeClassName="active-link" className={`py-2.5 px-4 text-gray-700 flex items-center gap-2 hover:rounded-tl-full hover:rounded-bl-full ${location.pathname === '/organisme' ? 'active-link' : ''}`}>
-          <img src={dashboardIcon} alt="Organisme" className="w-4 h-4" /> <span className="link-text">Organisme</span>
+          <img src={organismeIcon} alt="Organisme" className="w-4 h-4" /> <span className="link-text">Organisme</span>
         </NavLink>
       </nav>
       <div className="pb-6 pl-4 mt-auto">
